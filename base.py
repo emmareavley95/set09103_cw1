655d0d3eccd26520b18cd06039a7f137ca5f652e
Emma Reavley <emma.reavley@wanadoo.fr>
2016-10-19 18:05:56 +0000

    created all templates needed and render them together

--- base.py
+++ base.py
@@ -1,14 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+#from django.view.generic import RedirectView
+
 app = Flask(__name__)
+
+#urlpatterns = patterns('',
+ #   (r'^starter/$', RedirectView.as_view(url='/templates/starter.html'))
+#)
 
 @app.route('/')
 def root():
   return render_template('index.html'), 200
 
-@app.route('/#starter')
-def starters():
-  recipes = ['Mozzarella&Tomato salad', 'Chicken&Avocado wrap', 'French onion soup', 'Chicken Caesar salad']
-  return render_templates('starter.html', recipes=recipes)
+def RedirectView(request):
+ # recipes = ['Mozzarella&Tomato salad', 'Chicken&Avocado wrap', 'French onion soup', 'Chicken Caesar salad']
+  return render_template('starter.html')
+
+@app.route('/starter/')
+def starter():
+  return render_template('starter.html')
+
+@app.route('/meal/')
+def meal():
+  return render_template('meal.html')
+
+@app.route('/dessert/')
+def dessert():
+  return render_template('dessert.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+  return "We couldn't answer you request. Please try again later or this page might not exist.", 404
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
