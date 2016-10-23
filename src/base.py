3af8d8cb23cf9dbc798bffac1bd0368320f219ef
Emma Reavley <emma.reavley@wanadoo.fr>
2016-10-23 09:18:53 +0000

    webapp python file

--- src/base.py
+++ src/base.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,18 +6,31 @@ def root():
   recipes = ['Starter', 'Meal', 'Dessert']
   return render_template('index.html', recipes=recipes), 200
 
+@app.route('/test-image/')
+def test():
+  start = '<h2>Pasta</h2><img src="'
+  url = url_for('static', filename='img/pasta.jpg')
+  end = '">'
+  return  start+url+end, 200
+
 @app.route('/starter/')
 def starter():
-  starter = ['Chicken ceasar salad', 'French onion soup', 'Halloumi wrap', 'Guacamole']
+  starter = [dict(salad='Chicken caesar salad', img=url_for('static', filename='img/pasta.jpg')), 
+            dict(salad='French onion soup', img=url_for('static', filename='img/pasta.jpg')), 
+            dict(salad='Halloumi wrap', img=url_for('static', filename='img/pasta.jpg')), 
+            dict(salad='Guacamole', img=url_for('static', filename='img/pasta.jpg'))
+  ]
   return render_template('starter.html', starter=starter)
 
 @app.route('/meal/')
 def meal():
-  return render_template('meal.html')
+  meal = ['Quiche lorraine', 'Fish casserole', 'Sweet potato delight', 'Buckwheat pancakes']
+  return render_template('meal.html', meal=meal)
 
 @app.route('/dessert/')
 def dessert():
-  return render_template('dessert.html')
+  dessert = ['Crepes', 'American pancakes', 'Yogurt cake', 'Sunday caramel']
+  return render_template('dessert.html', dessert=dessert)
 
 @app.errorhandler(404)
 def page_not_found(error):
